20eb62db49056ece49e60cc67e45d940fe79d932
Pedro Moreno Chagas Ligeiro Nascimento <1161565+nennogabriel@users.noreply.github.com>
2023-10-23 21:49:07 +0000

    aula 1 database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,12 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query({
+    text: "SELECT 1 + 1 as sum",
+  });
+
+  console.log(result.rows);
+
   response.status(200).json({ status: "OK" });
 }
 
