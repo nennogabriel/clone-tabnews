b263459b249f63168ff68d1bd371e01aca2ebbf8
Pedro Moreno Chagas Ligeiro Nascimento <1161565+nennogabriel@users.noreply.github.com>
2023-11-30 20:29:46 +0000

    dia 20 - status returning max connections and open connections

--- integration/api/v1/status/get.test.js
+++ integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("Get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  const { updated_at, max_connections, open_connections } = responseBody;
+  expect(responseBody).toMatchObject({
+    updated_at: updated_at,
+    max_connections: expect.any(Number),
+    open_connections: expect.any(Number),
+  });
+  expect(max_connections).toBeGreaterThan(open_connections);
 });
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,13 +1,24 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query({
-    text: "SELECT 1 + 1 as sum",
+  const updatedAt = new Date().toISOString();
+  const reqMaxConnections = database.query({
+    text: "SHOW max_connections; ",
+  });
+  const reqCurrentConnections = database.query({
+    text: "SELECT COUNT(*) FROM pg_stat_activity; ",
   });
 
-  console.log(result.rows);
+  const result = await Promise.all([reqMaxConnections, reqCurrentConnections]);
 
-  response.status(200).json({ status: "OK" });
+  const maxConnections = Number(result[0].rows[0].max_connections);
+  const openConnections = Number(result[1].rows[0].count);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    max_connections: maxConnections,
+    open_connections: openConnections,
+  });
 }
 
 export default status;
