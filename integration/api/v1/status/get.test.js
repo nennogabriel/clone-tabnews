275a12f1fa8c4bf851933ac00507d3ed5ec5e8b8
Pedro Moreno Chagas Ligeiro Nascimento <1161565+nennogabriel@users.noreply.github.com>
2023-12-15 19:04:00 +0000

    refactoring to request once a time

--- integration/api/v1/status/get.test.js
+++ integration/api/v1/status/get.test.js
@@ -3,19 +3,19 @@ test("Get to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  const { updated_at, max_connections, open_connections, postgres_version } =
-    responseBody;
+
   expect(responseBody).toMatchObject({
     updated_at: expect.any(String),
-    postgres_version: expect.any(String),
-    max_connections: expect.any(Number),
-    open_connections: expect.any(Number),
+    dependencies: {
+      database: {
+        version: "16.0",
+        max_connections: expect.any(Number),
+        open_connections: 1,
+      },
+    },
   });
 
-  const parsedUpdatedAt = new Date(updated_at).toISOString(); // throws if invalid date
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString(); // throws if invalid date
 
-  expect(updated_at).toEqual(parsedUpdatedAt);
-
-  expect(max_connections).toBeGreaterThan(open_connections);
-  expect(Number(postgres_version)).toBeGreaterThanOrEqual(16);
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 });
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,29 +3,25 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const getPostgresVersion = database.query("SHOW server_version;");
-  const getMaxConnections = database.query("SHOW max_connections");
-  const getCurrentConnections = database.query(
-    "SELECT COUNT(*) FROM pg_stat_activity"
+  const getPostgresVersion = await database.query("SHOW server_version;");
+  const getMaxConnections = await database.query("SHOW max_connections");
+  const getCurrentConnections = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity where datname = 'local_db';"
   );
 
-  const result = await Promise.all([
-    getPostgresVersion,
-    getMaxConnections,
-    getCurrentConnections,
-  ]);
-
-  console.log(result[0].rows[0]);
-
-  const postgresVersion = result[0].rows[0].server_version;
-  const maxConnections = Number(result[1].rows[0].max_connections);
-  const openConnections = Number(result[2].rows[0].count);
+  const postgresVersion = getPostgresVersion.rows[0].server_version;
+  const maxConnections = parseInt(getMaxConnections.rows[0].max_connections);
+  const databaseOpenConnectionsValue = getCurrentConnections.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
-    postgres_version: postgresVersion,
-    max_connections: maxConnections,
-    open_connections: openConnections,
+    dependencies: {
+      database: {
+        version: postgresVersion,
+        max_connections: maxConnections,
+        open_connections: databaseOpenConnectionsValue,
+      },
+    },
   });
 }
 
