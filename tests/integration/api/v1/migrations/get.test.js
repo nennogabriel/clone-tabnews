ccc5dfcaa558f361012a65873da941bb5a54d8bb
Pedro Moreno Chagas Ligeiro Nascimento <1161565+nennogabriel@users.noreply.github.com>
2024-04-11 23:12:04 +0000

    jest mode run in band. plus chalange one

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,7 +1,16 @@
+import database from "infra/database";
+
 test("Get to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
+  console.log(`process.env.NODE_ENV = ${JSON.stringify(process.env.NODE_ENV)}`);
+  console.log(
+    `process.env.DATABASE_URL = ${JSON.stringify(process.env.DATABASE_URL)}`
+  );
+  ``;
+
   const responseBody = await response.json();
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
