e242ab0fdc2b7a643182fd1e3f4c8f08b204f9d5
Pedro Moreno Chagas Ligeiro Nascimento <1161565+nennogabriel@users.noreply.github.com>
2024-04-23 18:26:39 +0000

    fix `/migrations` endpoint connection bug

--- tests/integration/api/v1/migrations/delete.test.js
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -11,7 +11,7 @@ test("DELETE to /api/v1/migrations should return 404", async () => {
     method: "DELETE",
   });
 
-  expect(response1.status).toBe(404);
+  expect(response1.status).toBe(405);
 
   const response2 = await fetch("http://localhost:3000/api/v1/status");
   const response2Body = await response2.json();
