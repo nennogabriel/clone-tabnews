08b2fcc482a299d769d6780a1cb53a5a2d3b9e5e
Pedro Moreno Chagas Ligeiro Nascimento <1161565+nennogabriel@users.noreply.github.com>
2024-06-11 17:03:51 +0000

    makes `npm test` more robust with `async retry` and `orchestrator.js`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,11 +1,10 @@
 import database from "infra/database";
-// The below can be used in a Jest global setup file or similar for your testing set-up
+import orchestrator from "tests/orchestrator.js";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
-}
+});
 
 test("Get to /api/v1/migrations should return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
